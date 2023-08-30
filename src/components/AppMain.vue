a87faecdc49eaa935542e6d2bf9daf4b93ab454f
Gianluca-Basili <gianluc.basili@gmail.com>
2023-08-30 17:31:44 +0200

    add app main

---
+++ src/components/AppMain.vue
@@ -0,0 +1,47 @@
+<script>
+import axios from 'axios';
+export default {
+    name: 'AppMain',
+    data(){
+      return{
+            baseUrl: 'http://localhost:8000',
+            posts: [],
+            loading: true
+      }
+    },
+    created(){
+        this.getPosts();
+    },
+    methods:{
+        getPosts(){
+            this.loading = true;
+            axios.get( `${this.baseUrl}/api/posts`).then((response)=>{
+                
+                if(response.data.success){
+                    this.posts = response.data.results;
+                    this.loading = false;
+
+                }
+
+                else{
+
+                }
+            })
+        }
+    }
+}
+</script>
+<template lang="">
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center">Boolpress</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
