0dba87597f882b53a28c78b8ee7fd1ec3bbc377b
Gianluca-Basili <gianluc.basili@gmail.com>
2023-08-30 19:17:45 +0200

    add categroy and tags

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,7 +10,8 @@ export default {
       return{
             baseUrl:'http://localhost:8000',
             posts: [],
-            loading: true
+            loading: true,
+            maxNumCharacters: 250
       }
     },
     created(){
@@ -31,7 +32,17 @@ export default {
 
                 }
             })
+        },
+
+        truncateText(text){
+            if(text.length > this.maxNumCharacters ){
+                return text.substr(0,this.maxNumCharacters) + '...';
+            }
+            
+            return text;
         }
+
+
     }
 }
 </script>
@@ -47,13 +58,36 @@ export default {
         <AppLoader v-if="loading" />
         <div v-else class="container">
             <div class="row">
-                <div class="col-12" v-for="post in posts" :key="post.id">
-                        {{post.title}}
+                <div class="col-12 col-md-4 mt-5" v-for="post in posts" :key="post.id">
+                    <div class="card min-height-200">
+                        <div class="card-header">
+                            {{post.title}}
+
+                        </div>
+                        <div class="card-image-top">
+                            <img :src="`${baseUrl}/storage/${post.cover_image}`" class="img-fluid">
+                        </div>
+                        <div class="card-body">
+                            <p>
+                                <span v-if="post.category">{{post.category.name}}</span>
+                                <span v-else>Categoria non assegnata</span>
+                            </p>
+                            <p v-if="post.tags">
+                                <span class="badge text-bg-primary me-3" v-for="tag in post.tags" :key="tag.id">{{tag.name}}</span>
+                            </p>
+                            <p>{{truncateText(post.content)}}</p>
+                        </div>
+                        <div class="card-footer">
+                            <a href="#" class="btn btn-sm btn-primary">Leggi l'articolo</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
-<style lang="">
-    
+<style lang="scss">
+    .min-height-200{
+        min-height: 200px;
+    }
 </style>
